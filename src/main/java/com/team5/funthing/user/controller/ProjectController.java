c1a331177f4065b4af9db3fb7d307bea66aff883
genug <qparanoya@gmail.com>
2020-02-13 00:45:49 +0900

    프로젝트 Controller DAO VO Service 패키지 및 객체 생성

---
+++ src/main/java/com/team5/funthing/user/controller/ProjectController.java
@@ -0,0 +1,11 @@
+package com.team5.funthing.user.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProjectController {
+	
+	// 프로젝트  생성
+//	private CreateProjectService createProjectService;
+	
+}
---
+++ src/main/java/com/team5/funthing/user/service/projectService/InsertProject.java
@@ -0,0 +1,5 @@
+package com.team5.funthing.user.service.projectService;
+
+public interface InsertProject {
+
+}
