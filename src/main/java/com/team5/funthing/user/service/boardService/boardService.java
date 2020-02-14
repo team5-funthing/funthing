8114c117b0fe99e11004b164edf55e0673c100ed
ledad1029 <qotjdwlrdl@gmail.com>
2020-02-14 21:53:58 +0900

    공지사항 게시판 완료 - 버그수정만 남음 1.페이지번호가 음수일경우 처리 2. 페이지번호 선택후에 글검색시 안되는거 처리 3. 공지 알림, 이벤트게시판 부분도 해놓을 것

---
+++ src/main/java/com/team5/funthing/user/service/boardService/boardService.java
@@ -0,0 +1,18 @@
+package com.team5.funthing.user.service.boardService;
+
+import java.util.List;
+
+import com.team5.funthing.user.model.vo.BoardVO;
+import com.team5.funthing.user.model.vo.NoticeVO;
+
+public interface boardService {
+	
+	int selectBoardCount();
+	int calculateTotalPage(int noticeCountTotal);
+	List<NoticeVO> selectBoardEachPage(BoardVO vo);
+	int getStartRow(int currentPageNum);
+	int getEndRow(int currentPageNum);
+	int getStartPage(int currentPageNum);
+	int getEndPage(int startPageNum);
+	
+}
