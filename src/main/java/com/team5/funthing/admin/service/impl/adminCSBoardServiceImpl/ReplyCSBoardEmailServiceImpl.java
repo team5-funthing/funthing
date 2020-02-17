31b8436865dc84c3d3971f7d3528502742b9c2f1
mineyard <jungho.p47@gmail.com>
2020-02-17 21:50:31 +0900

    jhohojohoho

--- src/main/java/com/team5/funthing/admin/service/impl/adminCSBoardServiceImpl/ReplyCSBoardEmailServiceImpl.java
+++ src/main/java/com/team5/funthing/admin/service/impl/adminCSBoardServiceImpl/ReplyCSBoardEmailServiceImpl.java
@@ -27,8 +27,9 @@ public class ReplyCSBoardEmailServiceImpl implements ReplyCSBoardEmailService {
 	public void sendReplyEmail(AdminCSBoardVO avo,CSBoardVO cvo) throws UnsupportedEncodingException, MessagingException {
 		message = mailSender.createMimeMessage();
 		messageHelper = new MimeMessageHelper(message,true,"UTF-8");
-		//
+		
 		messageHelper.setSubject("[\""+cvo.getCsTitle()+"\"] [문의답변]:"+avo.getAdminCSTitle());
+		System.out.println("문의답변");
 		messageHelper.setText(avo.getAdminCSContent());
 		messageHelper.setFrom("funthing20@gmail.com",avo.getAdminID());
 		messageHelper.setTo(cvo.getEmail());
