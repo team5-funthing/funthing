880b88028a8712ba08b6b36d61a1b261eb11ecfa
mineyard <jungho.p47@gmail.com>
2020-02-17 21:56:28 +0900

    plz

--- src/main/java/com/team5/funthing/admin/service/adminCSBoardService/updateReplyCheckCSBoardService.java
+++
@@ -1,9 +0,0 @@
-package com.team5.funthing.admin.service.adminCSBoardService;
-
-import com.team5.funthing.user.model.vo.CSBoardVO;
-
-public interface UpdateReplyCheckCSBoardService {
-
-	void updateReplyCheckCSBoard(CSBoardVO vo);
-	
-}
