d50e5b893190a5d0ea5024804ef0c7c12cebc61f
CGW <ajoqwer@gmail.com>
2020-02-18 17:43:31 +0900

    다시 커밋...   멤버컨트롤러  인코딩 문제 수정.

--- src/main/java/com/team5/funthing/user/service/impl/memberServiceImpl/UpdateMemberServiceImpl.java
+++ src/main/java/com/team5/funthing/user/service/impl/memberServiceImpl/UpdateMemberServiceImpl.java
@@ -1,10 +1,10 @@
-package com.team5.funthing.user.memberServiceImpl;
+package com.team5.funthing.user.service.impl.memberServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.team5.funthing.user.memberService.UpdateMemberService;
 import com.team5.funthing.user.model.dao.MemberDAO;
+import com.team5.funthing.user.service.memberService.UpdateMemberService;
 
 @Service
 public class UpdateMemberServiceImpl implements UpdateMemberService{
--- src/main/java/com/team5/funthing/user/service/memberService/GetMemberService.java
+++ src/main/java/com/team5/funthing/user/service/memberService/GetMemberService.java
@@ -1,9 +1,9 @@
-package com.team5.funthing.user.memberService;
+package com.team5.funthing.user.service.memberService;
 
 import com.team5.funthing.user.model.vo.MemberVO;
 
 public interface GetMemberService {
-	 
 
 	public MemberVO getMember(MemberVO vo);
+	
 }
