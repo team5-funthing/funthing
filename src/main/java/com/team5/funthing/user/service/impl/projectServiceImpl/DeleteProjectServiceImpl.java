d00afac5fcec3bfe8dfc4f564eb91e10a6c83ad5
genug <qparanoya@gmail.com>
2020-02-26 13:15:03 +0900

    마이프로필 상세 페이지 관련.  - 만든 프로젝트 상태에 따른 뱃지 띄우기 구현  - 만든 프로젝트 수정 및 삭제 기능 구현

--- src/main/java/com/team5/funthing/user/service/impl/projectServiceImpl/DeleteProjectServiceImpl.java
+++ src/main/java/com/team5/funthing/user/service/impl/projectServiceImpl/DeleteProjectServiceImpl.java
@@ -1,16 +1,21 @@
 package com.team5.funthing.user.service.impl.projectServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.team5.funthing.user.model.dao.ProjectDAO;
+import com.team5.funthing.user.model.vo.ProjectVO;
 import com.team5.funthing.user.service.projectService.DeleteProjectService;
 
 @Service
 public class DeleteProjectServiceImpl implements DeleteProjectService {
 
+	@Autowired
+	private ProjectDAO projectDAO;
+	
 	@Override
-	public int deleteProject(int projectNo) {
-		// TODO Auto-generated method stub
-		return 0;
+	public void deleteProject(ProjectVO vo) {
+		projectDAO.deleteProject(vo);
 	}
 	
 	
