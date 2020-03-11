9eff334ad0ae3176cbb7c474e8bfcd81d9d599f0
ledad1029 <qotjdwlrdl@gmail.com>
2020-03-11 14:11:54 +0900

    좋아요 버튼을 누른 프로젝트 마이페이지에서 볼수있도록 구현완료 서비스 분리, 페이지 수정

--- src/main/java/com/team5/funthing/user/controller/MemberActivityController.java
+++ src/main/java/com/team5/funthing/user/controller/MemberActivityController.java
@@ -1,5 +1,6 @@
 package com.team5.funthing.user.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -10,13 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.team5.funthing.user.model.vo.MemberActivityVO;
 import com.team5.funthing.user.model.vo.MemberVO;
 import com.team5.funthing.user.model.vo.PaymentReserveVO;
 import com.team5.funthing.user.model.vo.ProjectVO;
 import com.team5.funthing.user.service.memberActivityService.DeleteMemberActivityService;
+import com.team5.funthing.user.service.memberActivityService.GetLikeProjectNoListService;
 import com.team5.funthing.user.service.memberActivityService.GetMemberActivityListService;
+import com.team5.funthing.user.service.memberActivityService.GetReportProjectNoListService;
+import com.team5.funthing.user.service.memberActivityService.GetReservationProjectNoListService;
 import com.team5.funthing.user.service.memberActivityService.InsertMemberActivityService;
 import com.team5.funthing.user.service.memberActivityService.UpdateMemberActivityService;
 import com.team5.funthing.user.service.paymentReserveService.GetPaymentReserveService;
@@ -26,22 +32,32 @@ import com.team5.funthing.user.service.projectService.GetProjectService;
 @Controller
 public class MemberActivityController {
 
+	//Project Service
+	@Autowired
+	private GetProjectListByEmailService getProjectServiceByEmailService;
+	@Autowired
+	private GetProjectService getProjectService;
+	
+	//PaymentReserve Service
+	@Autowired
+	private GetPaymentReserveService getPaymentReserveService;
+	
+	//MemberActivity Service
+	@Autowired
+	private InsertMemberActivityService insertMemberActivityService;
 	@Autowired
 	private DeleteMemberActivityService deleteMemberActivityService;
 	@Autowired
 	private GetMemberActivityListService getMemberActivityService;
 	@Autowired
-	private InsertMemberActivityService insertMemberActivityService;
-	@Autowired
 	private UpdateMemberActivityService updateMemberActivityService;
 	@Autowired
-	private GetProjectListByEmailService getProjectServiceByEmailService;
+	private GetLikeProjectNoListService getLikeProjectNoListService;
 	@Autowired
-	private GetProjectService getProjectService;
+	private GetReportProjectNoListService getReportProjectNoListService;
 	@Autowired
-	private GetPaymentReserveService getPaymentReserveService;
-
-
+	private GetReservationProjectNoListService getReservationProjectNoListService;
+	
 
 	@RequestMapping(value="mypage.udo",method=RequestMethod.GET)
 	public String myPage(HttpSession session, MemberActivityVO vo, ProjectVO vo1, Model model, MemberVO vo2) {
@@ -55,6 +71,28 @@ public class MemberActivityController {
 		return "p-detail-mypage";
 	}  
 	
+	@RequestMapping("upCountLike.udo")
+	public String upCountLike(MemberActivityVO mavo, 
+							Model model,
+							RedirectAttributes redirectAttributes,
+							@RequestParam(value="projectNo") String projectNo,
+							HttpSession session) {
+		
+		MemberVO clickLikeMember = (MemberVO) session.getAttribute("memberSession");
+		System.out.println(clickLikeMember.getEmail());
+		
+		mavo.setEmail(clickLikeMember.getEmail());
+		mavo.setProjectNo(Integer.parseInt(projectNo));
+		mavo.setLikeCount('y');
+		
+		System.out.println(mavo.toString());
+		
+		insertMemberActivityService.insertMemberActivity(mavo);
+		
+		redirectAttributes.addAttribute("projectNo", projectNo);
+		
+		return "redirect:projectDetails.udo"; //프로젝트 상세페이지
+	}
 
 	
 	
@@ -74,22 +112,28 @@ public class MemberActivityController {
 	
 
 	public void myLikeProjectList(HttpSession session,MemberActivityVO vo,ProjectVO vo1,Model model) {
-		List<ProjectVO> projectLikeList = new Vector<ProjectVO>();
-		MemberVO vo2 = (MemberVO) session.getAttribute("memberSession");
-		vo.setEmail(vo2.getEmail());
-		for(MemberActivityVO list:getMemberActivityService.getLikeProjectnoList(vo)) {
-			vo1.setProjectNo(list.getprojectno());
+		List<ProjectVO> projectLikeList = new ArrayList<ProjectVO>();
+		MemberVO loginMember = (MemberVO) session.getAttribute("memberSession");
+		//System.out.println("로그인 아이디 값 : " + loginMember.getEmail());
+		vo.setEmail(loginMember.getEmail());
+		//System.out.println("vo의 값 : " +  vo.toString());
+		List<MemberActivityVO> likeProjectList = getLikeProjectNoListService.getLikeProjectNoList(vo);
+		//System.out.println("회원이 좋아요 누른 거 : " + likeProjectList);
+		for(MemberActivityVO getProjectNo : likeProjectList) {
+			vo1.setProjectNo(getProjectNo.getProjectNo());
 			projectLikeList.add(getProjectService.getProject(vo1));
 		}
-		model.addAttribute("projectLikeList",projectLikeList);
+		//System.out.println(projectLikeList.toString());
+		
+		model.addAttribute("projectLikeList", projectLikeList);
 	}
 
 	public void myReportProjectList(HttpSession session,MemberActivityVO vo,ProjectVO vo1,Model model) {
 		List<ProjectVO> projectReportList = new Vector<ProjectVO>();
 		MemberVO vo2 = (MemberVO) session.getAttribute("memberSession");
 		vo.setEmail(vo2.getEmail());
-		for(MemberActivityVO list:getMemberActivityService.getReportProjectnoList(vo)) {
-			vo1.setProjectNo(list.getprojectno());
+		for(MemberActivityVO list: getReportProjectNoListService.getReportProjectNoList(vo)) {
+			vo1.setProjectNo(list.getProjectNo());
 			projectReportList.add(getProjectService.getProject(vo1));
 		}
 		model.addAttribute("projectReportList",projectReportList);
@@ -99,8 +143,8 @@ public class MemberActivityController {
 		List<ProjectVO> projectReservationList = new Vector<ProjectVO>();
 		MemberVO vo2 = (MemberVO) session.getAttribute("memberSession");
 		vo.setEmail(vo2.getEmail());
-		for(MemberActivityVO list:getMemberActivityService.getReservationProjectnoList(vo)) {
-			vo1.setProjectNo(list.getprojectno());
+		for(MemberActivityVO list: getReservationProjectNoListService.getReservationProjectNoList(vo)) {
+			vo1.setProjectNo(list.getProjectNo());
 			projectReservationList.add(getProjectService.getProject(vo1));
 		}
 		model.addAttribute("projectReservationList",projectReservationList);
